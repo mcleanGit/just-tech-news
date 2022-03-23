3ee2cfaec9d487e0b17860f62dfeeec3e634f116
Don McLean <mclean@utoronto.ca>
2022-03-23 15:26:47 -0400

    BCS check on config data

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to db, pass mySQL info username password
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize('just_tech_news_db', 'mclean@utoronto.ca', '02468a13579B!', {
  host: 'localhost',
  dialect: 'mysql',
  port: 3306

d2e6b94fbef709951526a5a6ab3a0b89aa136a22
Don McLean <mclean@utoronto.ca>
2022-03-23 15:19:13 -0400

    BCS check

--- models/User.js
+++ models/User.js
@@ -1,17 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
-const { underscoredIf } = require('sequelize/types/utils');
 const sequelize = require('../config/connection');
 
 // create User model
 class User extends Model {}
 
-// define table columns and cofig
+// define table columns and config
 User.init(
  {
-  // TABLE COLUMN DEFINITIONS GO HERE
- },
- {
-  // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))  
+	 id: {
+		 type: DataTypes.INTEGER,
+		 allowNull: false,
+		 primaryKey: true, 
+		 autoIncrement: true
+	 },
+	 username: {
+		 type: DataTypes.STRING,
+		 allowNull: false
+	 },
+	 email: {
+		 type: DataTypes.STRING,
+		 allowNull: false,
+		 unique: true,
+		 validate: {
+			 isEmail: true
+		 }
+	 },
+	 password: {
+		 type: DataTypes.STRING,
+		 allowNull: false,
+		 validate: {
+			 len: [4]
+		 }
+	 }
  },
  {
   // pass in imported sequelize connection (the direc connection to db)

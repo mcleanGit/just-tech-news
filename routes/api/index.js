45b7af079403e54ae6fb3b7e008c0d5f9ce5ed2e
Don McLean <mclean@utoronto.ca>
2022-03-23 15:20:33 -0400

    BCS check

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+// index.js file for API routes
+const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
+
+module.exports = router;

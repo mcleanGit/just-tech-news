bec6b5917b283814585a71c0eef58f52df3c4afd
Don McLean <mclean@utoronto.ca>
2022-03-24 12:07:27 -0400

    minor layout adjustment

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-	// expects {username: , email: , password: }
+	// expects { username: , email: , password: }
 	User.create({
 		username: req.body.username,
 		email: req.body.email,
